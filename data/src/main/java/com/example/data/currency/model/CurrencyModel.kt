d9738d81bb26c967bf19a15ed1e2c569e1747b47
gohar.ali <trigri1@gmail.com>
2020-04-12 21:56:00 +0300

    remove extra drawables

--- data/src/main/java/com/example/data/currency/model/CurrencyModel.kt
+++ data/src/main/java/com/example/data/currency/model/CurrencyModel.kt
@@ -1,7 +1,6 @@
 package com.example.data.currency.model
 
 import com.example.data.MappedModel
-import com.example.data.R
 
 data class CurrencyRateModel(
     val baseCurrency: String?, val currencyList: List<CurrencyModel>?
@@ -10,6 +9,5 @@ data class CurrencyRateModel(
 data class CurrencyModel(
     val currency: String?,
     var rate: Float = 1f,
-    val flag: Int = R.drawable.flag_eur,
     val currencyName: String?=null
 ) : MappedModel()
